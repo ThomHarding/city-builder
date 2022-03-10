0c168079edc2b5fbfd122365d0b71dc9a9a5b027
THOMAS HARDING <hardingt604@gmail.com>
2022-03-10 16:46:11 -0700

    added pseudocode of required js functions

--- app.js
+++ app.js
@@ -6,3 +6,27 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  //template for each dropdown:
+    //eventlistener for change
+      //get value of dropdown element
+      //change .src property of corresponding image; the first dropdown will alter imageOne etc
+
+      //count of that dropdown's changes will increment
+      //displayCountStats();
+      //grab slogan-input.value
+      //push to array of slogans
+      //displaySlogans();
+
+//displayCountStats function {
+  //grabs counters for changes to all three dropdowns
+  //add to proper format of output
+  //add formatted output to counts-holder div
+//}
+
+//displaySlogans function {
+  //clear innerText of slogan-holder div
+  //for each slogan in array of slogans
+    //create element containing slogan as innerText
+    //append that element as child to slogan-holder
+//}
